40803308261829fa87e7576c0332a2aaa6654c31
rliskunov <liskunov.roma@yandex.ru>
2021-06-19 12:56:42 +0300

    [no task] set doctor's page

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ('base', 'home', 'patient'):
+    for module_name in ('base', 'home', 'patient', 'doctor'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
